942588033c87da75082c63eb6c5b969e770eb22e
Ivan Reis <ivanreisdev@gmail.com>
2024-05-15 10:07:12 -0300

    :bug: fix: corrigindo bug qcritico de XSS

--- src/pages/Document/Document.jsx
+++ src/pages/Document/Document.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import style from './document.module.css'
+import React from 'react';
+import style from './document.module.css';
+import DOMPurify from 'dompurify';
 
 const Document = ({doc}) => {
-
+  // Sanitize the content
+  const sanitizedContent = DOMPurify.sanitize(doc.content);
 
   return (
     <div className={style.Documents}>
       <h2>{doc.nameDocs}</h2>
       <div className={style.DocumentContent}>
-        <div dangerouslySetInnerHTML={{ __html: doc.content }}></div>
+        <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Document
+export default Document;
