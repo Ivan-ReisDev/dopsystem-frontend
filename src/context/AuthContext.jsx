7c532217f1181195b4e369facb302ee5e4dc2b42
Ivan Reis <ivanreisdev@gmail.com>
2024-07-03 17:09:29 -0300

    Corrigindo erro

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,12 @@
 import { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import { Cookies } from 'universal-cookie';
 
 const PRD = 'https://dopsystem-backend.vercel.app/api/';
 const AuthContext = createContext('');
 const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
-    const cookies = new Cookies();
+
     const [isAuthentication, setIsAuthentication] = useState(false);
     const [authToken, setAuthToken] = useState(null);
     const [authProfile, setAuthProfile] = useState(null);
