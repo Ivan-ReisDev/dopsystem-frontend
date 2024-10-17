47ede0c9f6c7f5de1d5f73747a440cfe4822f8a1
Ivan Reis <ivanreisdev@gmail.com>
2024-10-17 20:08:37 -0300

    correção de bug

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -130,7 +130,7 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
 </ul>
 
         {infoProfileUserCompleted &&
-          (infoProfileUserCompleted.teams < 1 ||
+          (infoProfileUserCompleted.teams > 1 ||
             infoProfileUserCompleted.userType === "Admin" ||
             infoProfileUserCompleted.userType === "Diretor") && (
             <>
